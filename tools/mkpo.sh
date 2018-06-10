fea43b1dd9679c51be3e4e47d13deec365f08699
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2018-06-11 00:17:08 +0300

    Update translation files using msgmerge.

--- tools/mkpo.sh
+++ tools/mkpo.sh
@@ -4,4 +4,8 @@ PATH=/bin:/usr/bin; export PATH
 
 xgettext --keyword=_ --language=JavaScript --add-comments --sort-output -o ../translations/vkgeo.pot ../web/index.html ../web/vkgeo.js
 
-msginit --locale=ru --no-translator --input=../translations/vkgeo.pot --output=../translations/vkgeo_ru.po
+if [ -f ../translations/vkgeo_ru.po ]; then
+    msgmerge --update --backup=off ../translations/vkgeo_ru.po ../translations/vkgeo.pot
+else
+    msginit --locale=ru --no-translator --input=../translations/vkgeo.pot --output=../translations/vkgeo_ru.po
+fi
